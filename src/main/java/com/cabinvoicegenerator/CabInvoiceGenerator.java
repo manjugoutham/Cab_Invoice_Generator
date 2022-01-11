96b94f36994e49fdcfe3527d76e12bd3a60bdb83
Srinath S <srinath@Srinaths-MacBook-Air.local>
2022-01-11 20:02:42 +0530

    Added : Method to Check Totalfare of the journey

--- src/main/java/com/cabinvoicegenerator/CabInvoiceGenerator.java
+++ src/main/java/com/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -2,7 +2,16 @@ package com.cabinvoicegenerator;
 
 public class CabInvoiceGenerator {
 
+    public static final double COSTPerKM = 10.0;
+    public static final double COSTPerMIN = 1.0;
+    public static final double MIN_FARE = 5;
+
+    // UC-1 method to check the total fare for the journey
     public double calculateFare(double distance, int time) {
-        return 25;
+        double totalFare = (distance * COSTPerKM) + (time * COSTPerMIN);
+        if (totalFare < 5) {
+            return MIN_FARE;
+        }
+        return totalFare;
     }
 }
